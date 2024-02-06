36e05d242314ce95aa86fbfee68cd7cec5e15d23
Cyril El Hakim <elhakim.cyril@outlook.com>
2024-02-06 05:16:36 +0100

    Banner (jsx + scss) réalisée

--- Kasa/src/components/Banner.jsx
+++ Kasa/src/components/Banner.jsx
@@ -4,7 +4,10 @@ function Banner({img, title}) {
   return (
     <div className='banner'>
         <img src={img} alt="Image illustrant une falaise et une forêt" className='img-banner'/>
-        
+        {/*Affiche une image avec la source spécifiée par la prop img */}
+        {!title ? "": <h1 className="title">{title}</h1> }
+        {/*condition ternaire pour vérifier si la prop title est définie. 
+        Si prop title est défini, un élément h1 est rendu avec la classe CSS 'title' affichant le titre. Sinon, rien n'est rendu à cet endroit*/}
     </div>
   )
 }
