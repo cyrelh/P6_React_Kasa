208f45d05e08416e8541c4c3b3d986172de1aa5e
Cyril El Hakim <elhakim.cyril@outlook.com>
2024-02-06 04:08:55 +0100

    Header (scss +jsx) réalisé correspondant au logo et création de liens de navigation réactifs en utilisant NavLink pointant vers la page accueil par "/" ou  à propos par "/about"

--- Kasa/src/components/Header.jsx
+++ Kasa/src/components/Header.jsx
@@ -1,12 +1,20 @@
+import { NavLink } from 'react-router-dom'// Importation du composant NavLink depuis 'react-router-dom'
 import Kasa from '../assets/logo-kasa.png'
 import '../styles/Header.scss'
+
 
 function Header() {
   return (
     <header className='header'>
         <div className='under-header'>
-            <img src={Kasa} alt="Logo de Kasa" className='logo-kasa' />
-
+            <img src={Kasa} alt="Logo de Kasa" className='logo-kasa' />  {/* Balise img pour afficher le logo Kasa avec class CSS */}
+            <nav className="nav-links">
+                    <NavLink className={({ isActive }) => isActive ? "active" : "accueil"} to="/" >Accueil</NavLink> 
+                    {/*isActive = fonction qui retourne true si le lien est actif, cad s'il est au chemin de l'URL actuel. */}
+                    {/*Si le lien est actif, la classe "active" est appliquée ; sinon, la classe "accueil"  */}
+                    <NavLink className={({ isActive }) => isActive ? "active" : "propos"} to="/about">A Propos</NavLink>
+                    {/*Utilisation de NavLink pour créer des liens de navigation réactifs. Le 1er NavLink => page d'accueil et le 2nd => "A Propos".*/}
+            </nav>
         </div>
         
     </header>
--- Kasa/src/main.jsx
+++ Kasa/src/main.jsx
@@ -1,9 +1,16 @@
+// Importation de React depuis la bibliothèque 'react'
 import React from 'react'
+// Importation de ReactDOM depuis le module 'react-dom/client'
 import ReactDOM from 'react-dom/client'
+// Importation du composant fonctionnel principal App depuis le chemin spécifié
 import App from '../src/components/App.jsx'
 
+
+// Création d'une racine React à l'élément avec l'ID 'root' dans le document
+// Cette méthode crée une racine React pour le rendu, ce qui permet une mise à jour efficace du DOM
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  //mode Strict de React pour détecter les problèmes potentiels dans le code
+  <React.StrictMode> 
     <App />
   </React.StrictMode>,
 )
