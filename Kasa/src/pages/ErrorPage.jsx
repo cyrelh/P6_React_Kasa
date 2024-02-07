5c4abc9d8ca378b08acfc00d1caf80d58455404d
Cyril El Hakim <elhakim.cyril@outlook.com>
2024-02-07 01:44:20 +0100

    Début création Page Error 404 avec import des composants header et footer et import de NavLink depuis 'react-router-dom'

--- Kasa/src/pages/ErrorPage.jsx
+++ Kasa/src/pages/ErrorPage.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import Header from '../components/Header.jsx'
+import Footer from '../components/Footer.jsx'
+import '../styles/ErrorPage.scss'
+import { NavLink } from 'react-router-dom'
 
 function ErrorPage() {
   return (
-    <div>ErrorPage</div>
+    <div>
+      <Header />
+        <div className='error'>
+          <div className='404'>404</div>
+          <div className='oups'><p>Oups! La page que vous demandez n'existe pas.</p></div>
+          <NavLink to='/' className='error-accueil'>Retourner sur la page d’accueil</NavLink>   {/* Définition d'un lien de redirection vers la page d'accueil */}
+
+        </div>
+      <Footer/>
+      
+    </div>
   )
 }
 
