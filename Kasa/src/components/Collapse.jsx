2703f103a65d2cc8581b3d6c758b1cae588ded7b
Cyril El Hakim <elhakim.cyril@outlook.com>
2024-02-07 06:52:04 +0100

    Création de la page à propos fonctionnelle réalisée

---
+++ Kasa/src/components/Collapse.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react' // Import de useState depuis React pour gérer l'état du composant
+import iconCollapse from "../assets/collapse.png" // Import de l'icône collapse depuis les ressources
+import "../styles/Collapse.scss"
+
+function Collapse({name, para}) { // Définition du composant Collapse avec les props {name, para}
+    const [isOpen, setIsOpen] = useState(false)     // Déclaration d'un état local 'isOpen' pour suivre l'état d'ouverture ou de fermeture du contenu
+        return (
+            <>
+            <div className="all-collapse">             {/* Conteneur principal du Collapse */}
+                <div className="collapse-content">     {/* Contenu du Collapse */}
+                    <div className="collapse-banner">  {/* Bannière du Collapse */}
+                        <div className="collapse-banner-name">   {/* Nom du Collapse */}
+                            <p>{name}</p> {/* Affichage du nom de la section */}
+                            <img src={iconCollapse} // Définit la source de l'image de l'icône de collapse
+                            alt={isOpen ? "collapse" : "uncollapse"} //Si isOpen est vrai, alors contenu est ouvert, donc l'alt est "collapse". Sinon, l'alt est "uncollapse"
+                            className={isOpen ? "collapse" : "uncollapse"} // Si isOpen est vrai, la classe est "collapse", sinon c'est "uncollapse". Ces classes  utilisées pour appliquer des styles à l'icône en fonction de son état
+                            onClick={() => setIsOpen(!isOpen)} /> 
+                            {/* gestionnaire d'événements pour clic sur l'icône. 
+                            Lorsque l'utilisateur clique sur l'icône, cette fonction est appelée pour inverser l'état d'ouverture (isOpen)
+                            Cela signifie que si isOpen est vrai, il devient faux, et vice versa. */}
+                        </div>
+                    </div>
+                    {/* Contenu rétractable du Collapse */}
+                    <div className={isOpen ? "content show" : "content"}>
+                        {/* Si para est une chaîne de caractères, affiche-la */}
+                        {typeof para === "string" && (
+                            <p>{para}</p>
+                        )}
+                     {/* . Si para est effectivement un tableau, alors le code à l'intérieur de la condition est exécuté. */}
+                        {Array.isArray(para) && (
+                            para.map((item, index) => ( // Pour chaque élément item du tableau, une fonction fléchée est utilisée pour générer un paragraphe <p> qui affiche cet élément
+                                <p key={item + index}>{item}</p> 
+                                //on crée paragraphe pour chaque élément du tableau para. 
+                                //La key = la concaténation de l'élément item et index ce qui garantit que chaque élément a une clé unique
+                                //Cette clé va aider React à identifier chaque élément lors de la màj de l'interface utilisateur 
+                            ))
+                        )}
+                    </div>
+                </div>
+            </div>
+            </>
+            ) 
+}
+
+export default Collapse
--- Kasa/src/pages/About.jsx
+++ Kasa/src/pages/About.jsx
@@ -1,18 +1,46 @@
 import Header from '../components/Header.jsx'
 import Banner from '../components/Banner.jsx'
-import ImgAboutBanner from '../assets/about-banner.png'
+import ImgAboutBanner from '../assets/about-banner.png' // Import de l'image de la bannière
 import Footer from '../components/Footer.jsx'
+import Collapse from '../components/Collapse.jsx' //Import du composant Collapse
+
+
 
 
 function About() {
   return (
     <div>    <Header/>
     <Banner img={ImgAboutBanner}/>
+    <div className='conteneur-collapse'> {/* Début de la div pour contenir les composants Collapse */}
+      {data.map((item) => ( // on utilise la méthode map pour parcourir chaque élément du tableau data
+        <Collapse key={item.name} name={item.name} para={item.reponse} /> // Pour chaque élément, on crée composant Collapse en passant le nom de la section (item.name) comme titre et le contenu de la section (item.reponse) comme propriété au composant.
+      ))}
+    </div>
 
     <Footer/>
     </div>
 
   )
 }
+// Définition des données à afficher dans les composants Collapse
+const data = [
+  {
+    name: 'Fiabilité',
+    reponse:'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.'
+  },
+  {
+    name: 'Respect',
+    reponse:'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'
+  },
+  {
+    name: 'Service',
+    reponse:'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'
+  },
+  {
+    name: 'Sécurité',
+    reponse:"La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+  }
+
+]
 
 export default About
