cc074a0f69242b05809691bdb10d91f99be4a243
Cyril El Hakim <elhakim.cyril@outlook.com>
2024-02-13 08:31:32 +0100

    Implémentation de la page ApartmentPage avec son composant Slideshow en jsx réalisée + début du rating

--- Kasa/src/components/Slideshow.jsx
+++ Kasa/src/components/Slideshow.jsx
@@ -1,11 +1,53 @@
+// On importe les dépendances nécessaires
 import React from 'react'
+import { useState } from "react"
 import ArrowBack from '../assets/arrow_back.png'
 import ArrowForward from '../assets/arrow_forward.png'
 
-function Slideshow() {
+function Slideshow(slides) { //Déclaration du composant fonctionnel 
+  const [currentIndex, setCurrentIndex] = useState(0); // [currentIndex, setCurrentIndex] :"destructuring" --> déclare respectivement la valeur actuelle de l'état et une fonction pour mettre à jour cette valeur
+  // hook useState(0) : Cela initialise la variable d'état currentIndex avec une valeur initiale de 0. Cela signifie que lorsque le composant est monté pour la première fois, currentIndex aura la valeur 0.
+
+
+  const ToPrevious = () => { // Fonction pour passer à la diapositive précédente
+      const isFirstSlide = currentIndex === 0 // variable booléenne qui Vérifie si la diapositive actuelle est la première
+      const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1 
+      //Si isFirstSlide TRUE, diapositive actuelle est la première diapositive. Donc, newIndex est défini comme l'index de la dernière diapositive. 
+      //slides.length donne le nombre total de diapositives, en faisant - 1, on obtient l'index de la dernière diapositive car les indices commencent à 0
+
+      // Si isFirstSlide FALSE, diapositive actuelle n'est pas la première diapositive. Donc, newIndex est défini comme l'index de la diapositive précédente par rapport à la diapositive actuelle. 
+      // Pour cela, on décrémente simplement currentIndex de 1 pour obtenir l'index de la diapositive précédente.
+      setCurrentIndex(newIndex) // Met à jour l'index de la diapositive actuelle
+  }
+
+  const ToNext = () => { // Fonction pour passer à la diapositive suivante
+      const isLastSlide = currentIndex === slides.length - 1 // vérifie si la diapositive actuelle est la dernière dans le tableau slides
+      //currentIndex c'est l'index de la diapo actuellement affichée dans le diapo
+      //slides.length - 1 donne l'index de la dernière diapositive dans le tableau slides, car les indices commencent à zéro. Donc, slides.length - 1 est l'index de la dernière diapositive dans le tableau.
+      // Si currentIndex est = à l'index de la dernière diapositive, alors isLastSlide sera True, sinon, ce sera False.
+
+      const newIndex = isLastSlide ? 0 : currentIndex + 1 //définit newIndex cô l'index de la diapositive suivante à afficher dans le diaporama
+      //Si isLastSlide TRUE, alors diapo actuelle est la dernière dans le diaporama. Alors, on revient à la première diapo, donc newIndex est défini à 0.
+      //Si isLastSlide FALSE, alors diapo actuelle n'est pas la dernière dans le diaporama. Alors, on passe à la diapo suivante, donc newIndex est défini en incrémentant currentIndex de 1 pour avoir index de la diapo suivante
+      setCurrentIndex(newIndex)
+  }
+
+  if(slides.length === 1) { // Si le tableau de diapositives ne contient qu'une seule diapositive
+      return (
+          <div className="slider-scss">
+              <div className="slide-scss" style={{backgroundImage: `url(${slides[currentIndex]})`}}></div> {/* propriété de style en ligne qui définit l'image de background en utilisant la diapositive actuelle */}
+               {/* récupère l'URL de l'image de la diapositive actuelle à partir du tableau slides, en utilisant l'index currentIndex. */}
+          </div>
+      )
+  } else {  // Si le tableau de diapositives contient plusieurs diapositives
   return (
-    <div>Slideshow</div>
-  )
+      <div className="slider-scss">
+          <div className="counter">{currentIndex + 1}/{slides.length}</div> {/* Affiche le numéro de la diapositive actuelle */}
+          <img src={ArrowBack} alt="left" className="left" onClick={ToPrevious}/> {/* Bouton pour passer à la diapositive précédente */}
+          <img src={ArrowForward} alt="right" className="right" onClick={ToNext}/> {/* Bouton pour passer à la diapositive suivante */}
+          <div className="slide-scss" style={{backgroundImage: `url(${slides[currentIndex]})`}}></div> {/* Affiche la diapositive actuelle */}
+      </div>
+  )}
 }
 
 export default Slideshow
--- Kasa/src/pages/ApartmentPage.jsx
+++ Kasa/src/pages/ApartmentPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import Slideshow from '../components/Slideshow.jsx'
-import Collapse from '../components/Collapse.jsx'
-import ErrorPage from '../pages/ErrorPage.jsx'
 import '../styles/ApartmentPage.scss'
+import Collapse from '../components/Collapse.jsx'
+import ErrorPage from './ErrorPage.jsx'
+import Rating from "../components/Rating.jsx"
 
 
 
@@ -11,26 +13,73 @@ function ApartmentPage() {
   const [logement, setLogement] = useState({}) // on déclare un état local nommé logement avec une valeur initiale de {} (un objet vide)
   //logement va contenir les données récupérées à partir de la requête fetch
   
-  const {id} = useParams(); //onction useParams() fournie par React Router pour extraire les paramètres d'URL
-  //Dans ce cas, id est extrait des paramètres d'URL. Par exemple, si l'URL est "/logement/123", id contiendra la valeur "123"
+  const {id} = useParams(); // hook useParams permet d'extraire les paramètres d'URL
+  //ici id est extrait des paramètres d'URL. Par exemple, si l'URL est "/logement/123", id contiendra la valeur "123"
     useEffect(() => { // ce hook est déclenché chaque fois que la valeur de id change
-      //Dans ce cas, il déclenche une requête fetch pour récupérer les données du fichier JSON "/logements.json" en fonction de l'ID extrait de l'URL
+      //Dans ce cas, il déclenche une requête fetch pour récupérer les données du fichier "/logements.json" en fonction de l'ID extrait de l'URL
         fetch("/logements.json")
-        .then(response => response.json())
-        .then((data) => { //Cela reçoit les données JSON et cherche l'objet logement correspondant à l'ID extrait de l'URL
-          let log = data.find(l => l.id === id) //Une fois l'objet trouvé, il est stocké dans la variable log
+        .then(response => response.json()) // Convertit la réponse en JSON
+        .then((data) => {  // Une fois les données récupérées
+          let log = data.find(l => l.id === id) // Trouve l'objet logement correspondant à l'ID extrait de l'URL
           console.log(log) 
-          setLogement(log) //et mis à jour dans l'état logement à l'aide de setLogement(log)
+          setLogement(log) //Met à jour l'état logement avec les données trouvées à l'aide de setLogement(log)
         })
-        .catch((error) => console.error(error))
-    },[id]);
+        .catch((error) => console.error(error)) // Gère les erreurs de requête
+    },[id]); // Déclenche l'effet uniquement lorsque l'ID change
 
-    if(logement === undefined) { //Si l'objet logement est indéfini (si aucun logement correspondant à l'ID n'est trouvé dans les données récupérées), 
-      return (<><ErrorPage /></>) // le composant rend <ErrorPage />
-    } else { // sinon, il continue à rendre le composant avec les données de logement récupérées
+    if(logement === undefined) { //Si aucun logement correspondant à l'ID n'est trouvé
+      return (<><ErrorPage /></>); // Renvoie la page d'erreur 
+    } else { // Sinon, affiche les données du logement
 
   return (
     <div>
+            <div className="conteneur">
+              <div className="conteneur-scss"> 
+                {logement && logement.pictures && (<Slideshow slides={logement.pictures}/> // si logement est défini et si contient une propriété pictures, le composant Slideshow sera rendu avec les images spécifiées dans logement.pictures
+                //(opé logique && qui retourne la dernière valeur évaluée ==> si logement est défini et s'il contient une propriété pictures
+                //Si c'est le cas, l'expression retourne la valeur de logement.pictures ) -->donc si true cela rend le composant Slideshow avec les propriétés slides définies sur logement.pictures
+                )} 
+              </div>
+            </div>
+            <main>
+              <div className="title-location-tags">
+                <div className="title">
+                  <h2>{logement && logement.title}</h2> {/* affiche un élément <h2> avec le titre du logement uniquement si l'objet logement est défini ET s'il contient une propriété title */}
+                </div>
+                <div className="location">
+                  <p>{logement && logement.location}</p> {/* affiche <p> avec l'emplacement du logement uniquement si l'objet logement est défini ET s'il contient une propriété location */}
+                </div>
+                <div className="tags">
+                  {logement && logement.tags && logement.tags.map(tag => {// afiche liste de tags pour le logement uniquement si l'objet logement est défini, s'il contient une propriété tags, 
+                  //Et si logement.tags est un tableau. SI TOUTES conditions true  alors méthode map itère sur chaque élément du tableau logement.tags
+                    return (
+                      <div className="tag" key={tag}>{tag}</div> //Pour chaque tag, map retourne un élément <div> avec la classe tag qui a le tag en tant que contenu ET ayant une clé unique key définie comme tag
+                    )
+                  })}
+                </div>
+              </div>
+              <div className="host-rating">
+                <div className="host-name-picture">
+                  <div className="host-name">
+                    <p>{logement && logement.host && logement.host.name}</p> {/* affiche <p> avec le nom de l'hôte uniquement si objet logement est défini, s'il contient une propriété host, 
+                    ET si logement.host est défini */}
+                  </div>
+                  <div className="host-picture">
+                    <img src={logement && logement.host && logement.host.picture} alt="profile-picture" /> {/* affiche <img> avec l'image de profil de l'hôte uniquement si l'objet logement est défini, 
+                    s'il contient une propriété host, et si logement.host est défini. */}
+                  </div>
+                </div>
+                <div className="rating">
+                  <Rating rate={logement && logement.rating} /> {/* affiche composant Rating avec la note attribuée au logement uniquement si l'objet logement est défini et s'il contient une propriété rating. */}
+                </div>
+              </div>
+            </main>
+            <div className="description-equipements">
+              <Collapse name="Description" para={logement && logement.description}/> {/* Cela vérifie si l'objet logement est défini. 
+              Si c'est le cas, il vérifie si logement a une propriété description. Si c'est TRUE, l'expression renvoie la valeur de logement.description, sinon elle renvoie FALSE ou undefined */}
+              {/* Si l'expression précédente est évaluée à TRUE, alors affiche composant Collapse avec le nom "Description" et la description du logement. Sinon, rien n'est rendu */}
+              <Collapse name="Équipements" para={logement && logement.equipments }/>
+            </div>
 
     </div>
   )
