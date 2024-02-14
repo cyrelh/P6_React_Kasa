314603c5600e70e7f9f80d99fad8bd14c8e66000
Cyril El Hakim <elhakim.cyril@outlook.com>
2024-02-14 08:50:10 +0100

    Ajout création Rating stars

--- Kasa/src/components/Rating.jsx
+++ Kasa/src/components/Rating.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-function Rating() {
+import ActiveStar from '../assets/star-active.png' 
+import InactiveStar from '../assets/star-inactive.png'
+
+function Rating({rate}) {
+  const panel = [
+    1,2,3,4,5
+  ]
   return (
-    <div>Rating</div>
+    <div>
+      {panel.map((panelNumber) =>
+            rate >= panelNumber ? (<img key={`rating-${panelNumber.toString()}`} 
+            src={ActiveStar} alt="activestar" className="activestar"/>) : (<img key={`rating-${panelNumber.toString()}`} 
+            src={InactiveStar} alt="inactivestar" className="inactivestar"/>)
+            )}
+    </div>
   )
 }
 
