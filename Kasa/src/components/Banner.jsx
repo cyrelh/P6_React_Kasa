1f9c46b3f1ef602ef271b7e1b294e09b8dfd2224
Cyril El Hakim <elhakim.cyril@outlook.com>
2024-02-29 15:14:29 +0100

    RESOLU correction composant Banner div remplacé par un span car ça n'etait pas valide W3C à cause d'un <div> dans <h1>

--- Kasa/src/components/Banner.jsx
+++ Kasa/src/components/Banner.jsx
@@ -4,37 +4,17 @@ function Banner({img, title}) {
   return (
     <div className='banner'>
         <img src={img} alt="Image illustrant une falaise et une forêt" className='img-banner'/>
-        {/* Affiche une image avec la source spécifiée par la prop img */}
         {!title ? "" : (
           <h1 className="title">
-            {/* Affiche "Chez vous," et revient à la ligne, puis affiche "partout et ailleurs" */}
             {title.includes("Chez vous,") ? (
-              <div className="title-text">
+              <span className="title-text"> {/* Utilisation de <span> à la place de <p> */}
                 Chez vous,<br />
                 {title.split("Chez vous,")[1]}
-              </div>
+              </span>
             ) : title}
           </h1>
         )}
-        {/* Condition ternaire pour vérifier si la prop title est définie. 
-            Si prop title est défini, un élément h1 est rendu avec la classe CSS 'title' affichant le titre. Sinon, rien n'est rendu à cet endroit */}
     </div>
   );
 }
-
 export default Banner;
-
-
-// function Banner({img, title}) {
-//   return (
-//     <div className='banner'>
-//         <img src={img} alt="Image illustrant une falaise et une forêt" className='img-banner'/>
-//         {/*Affiche une image avec la source spécifiée par la prop img */}
-//         {!title ? "": <h1 className="title">{title}</h1> }
-//         {/*condition ternaire pour vérifier si la prop title est définie. 
-//         Si prop title est défini, un élément h1 est rendu avec la classe CSS 'title' affichant le titre. Sinon, rien n'est rendu à cet endroit*/}
-//     </div>
-//   )
-// }
-
-// export default Banner
